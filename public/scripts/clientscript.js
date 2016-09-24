3480d56947eb3e1c7d16afd72aafd92a3bfa6ae3
John Arkema <john.arkema@gmail.com>
2016-09-23 22:50:20 -0500

    controller edit

--- public/scripts/clientscript.js
+++ public/scripts/clientscript.js
@@ -1,47 +1,50 @@
-console.log('js');
+  console.log('js');
 
-var myApp = angular.module('myApp', ['ngRoute']);
+  var myApp = angular.module('myApp', ['ngRoute']);
 
-var allStudents= [];
+  var allStudents= [];
 
-myApp.controller('newStudent',['$scope', '$http',function($scope,$http){
-console.log('NG');
-$scope.allStudents = [];
-$scope.addNewStudent = function(){
-var newStudent ={
-  assignment_number: $scope.assignmentNumber,
-  student_name: $scope.studentName,
-  score: $scope.studentScore,
-  date_completed: $scope.completedDate
-};//end newStudent object to send
+  myApp.controller('newStudent',['$scope', '$http',function($scope,$http){
+  console.log('NG');
 
-$http({
-method: 'POST',
-url: '/create',
-data: newStudent
+  $scope.allStudents = [];
 
-}).then(function(response){
-  console.log('back from server sir ', response);
-});
-$scope.allStudents.push(newStudent);
+  $scope.addNewStudent = function(){
 
-};//end add super hero scope
+  var newStudent ={
+    assignment_number: $scope.assignmentNumber,
+    student_name: $scope.studentName,
+    score: $scope.studentScore,
+    date_completed: $scope.completedDate
+  };//end newStudent object to send
 
+  $http({
+  method: 'POST',
+  url: '/create',
+  data: newStudent
 
-}]);//end myApp controller
-
-
-myApp.config(['$routeProvider',function ($routeProvider){
-  $routeProvider.
-  when ("/home",
-    {templateUrl: "partials/home.html",
-    controller: "homeController"
-  }).
-  when ("/getAll",
-    {templateUrl: "partials/getAll.html",
-    controller: "getAllController"
-  }).
-  otherwise({
-    redirectTo: "/"
+  }).then(function(response){
+    console.log('back from server sir ', response);
   });
-}]);
+  $scope.allStudents.push(newStudent);
+
+  };//end add super hero scope
+
+
+  }]);//end myApp controller
+
+
+  myApp.config(['$routeProvider',function ($routeProvider){
+    $routeProvider.
+    when ("/home",
+      {templateUrl: "partials/home.html",
+      controller: "homeController"
+    }).
+    when ("/getAll",
+      {templateUrl: "partials/getAll.html",
+      controller: "getAllController"
+    }).
+    otherwise({
+      redirectTo: "/"
+    });
+  }]);
